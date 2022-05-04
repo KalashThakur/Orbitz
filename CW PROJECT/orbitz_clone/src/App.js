194f8bde07c595ab0d828c26b4a360e882a00743
Kalash Thakur <83134042+KalashThakur@users.noreply.github.com>
2022-05-04 18:36:25 +0530

    hotel description page

--- CW PROJECT/orbitz_clone/src/App.js
+++ CW PROJECT/orbitz_clone/src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Home from './components/Home';
 import NationalParks from './components/NationalParks';
+import F1 from './components/F1';
 
 function App() {
   return (
     <div className="App">
       {/* <Home /> */}
-      <NationalParks />
+      {/* <NationalParks /> */}
+      <F1 />
     </div>
   );
 }
