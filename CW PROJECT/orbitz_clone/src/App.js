cff69642c7be8180fce1a6523821bf3a4f4b2a0a
Kalash Thakur <83134042+KalashThakur@users.noreply.github.com>
2022-05-05 15:12:10 +0530

    additional data

--- CW PROJECT/orbitz_clone/src/App.js
+++ CW PROJECT/orbitz_clone/src/App.js
@@ -3,13 +3,16 @@ import './App.css';
 import Home from './components/Home';
 import NationalParks from './components/NationalParks';
 import F1 from './components/F1';
+import Description from './components/Description';
+
 
 function App() {
   return (
     <div className="App">
       {/* <Home /> */}
       {/* <NationalParks /> */}
-      <F1 />
+      {/* <F1 /> */}
+      <Description />
     </div>
   );
 }
---
+++ CW PROJECT/orbitz_clone/src/components/Description.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import data from "../data.json";
+
+const description = () => {
+    console.log(data)
+  return (
+    <div>
+    </div>
+  )
+}
+
+export default description
