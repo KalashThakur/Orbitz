1e5350846fefcfde296bd21d58910a9d4d59202c
abhishekskmr44 <abhishekskmr44@gmail.com>
2022-05-07 20:35:51 +0530

    hello

--- CW PROJECT/orbitz_clone/src/App.js
+++ CW PROJECT/orbitz_clone/src/App.js
@@ -13,7 +13,7 @@ function App() {
   return (
     <div className="App">
       <Home />
-      {/* <NationalParks /> */}
+      <NationalParks />
       {/* <F1 /> */}
       {/* <Description /> */}
       {/* <PlayStore /> */}
